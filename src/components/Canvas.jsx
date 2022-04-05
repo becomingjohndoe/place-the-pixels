84065b4d1db96f70d9bd7c7e49b4a2fa3963e841
becomingjohndoe <karl.swatman@gmail.com>
2022-04-05 15:03:25 +0100

    enable placing of pixels

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,32 @@
 import { useState } from "react";
 
 export default function Canvas() {
+	const [pixel, setPixel] = useState([]);
 	return (
-		<div className="grid overflow-auto w-auto relative">
-			<div className="h-[1000px] w-[1000px] border mx-10">
-				<div className="absolute top-[100px] bg-slate-700 h-[5px] w-[5px]"></div>
+		<div className="grid overflow-auto relative">
+			<div
+				onClick={(e) => {
+					console.log(e.clientX / 10, e.clientY / 10);
+					setPixel([
+						...pixel,
+						{
+							x: Math.floor(e.clientX / 10) * 10,
+							y: Math.floor(e.clientY / 10) * 10,
+							color: "bg-slate-700",
+						},
+					]);
+				}}
+				className="h-[1000px] w-[1000px] border m-10"
+			>
+				{pixel.map(({ x, y, color }) => (
+					<div
+						style={{
+							top: y,
+							left: x,
+						}}
+						className={`absolute ${color} h-[10px] w-[10px]`}
+					></div>
+				))}
 			</div>
 		</div>
 	);
--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Color() {
-	return <div className="w-screen h-20 bg-slate-600 overflow-hidden">Color</div>;
+	return (
+		<div className="w-screen h-12 bg-slate-600 overflow-hidden fixed bottom-0 ">
+			Color
+		</div>
+	);
 }

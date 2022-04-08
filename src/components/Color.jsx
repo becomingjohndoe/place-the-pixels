2436e8efb536be1c5a949ad5c764714a117a7ed8
becomingjohndoe <karl.swatman@gmail.com>
2022-04-08 18:06:32 +0100

    list of colors

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 
+const colors = [
+	"#ff4500",
+	"#ffa800",
+	"#facc15",
+	"#ffd635",
+	"#00a268",
+	"#7eed56",
+	"#2450a4",
+	"#3690ea",
+	"#51e9f4",
+	"#811e9f",
+	"#b44ac0",
+	"#ff99aa",
+	"#9c6926",
+	"#000000",
+	"#898d90",
+	"#d4d7d9",
+	"#ffffff",
+];
+
 export default function Color({ setSelectedColor }) {
 	return (
-		<div className="flex justify-center w-screen h-12 bg-slate-50 shadow-2xl shadow-black overflow-hidden fixed bottom-0 row-span-1">
-			<div className="flex justify-center items-center ">
-				<button
-					onClick={() => setSelectedColor("#4ade80")}
-					className="bg-green-400 border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1"
-				></button>
-				<button
-					onClick={() => setSelectedColor("black")}
-					className="bg-slate-700 border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1 "
-				></button>
-				<button
-					onClick={() => setSelectedColor("yellow")}
-					className="bg-yellow-400 border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1"
-				></button>
-				<button
-					onClick={() => setSelectedColor("red")}
-					className="bg-red-400 border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1"
-				></button>
-				<button
-					onClick={() => setSelectedColor("blue")}
-					className="bg-blue-400 border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1"
-				></button>
+		<div className="flex justify-center w-screen h-12 bg-slate-50 shadow-2xl shadow-black  fixed bottom-0 left-0">
+			<div className="flex justify-center items-center overflow-scroll">
+				{colors.map((color) => (
+					<button
+						onClick={() => setSelectedColor(color)}
+						style={{ backgroundColor: color }}
+						className="border h-8 w-14 focus:border-5 focus:border-slate-700 focus:-translate-y-1"
+					></button>
+				))}
 			</div>
 		</div>
 	);
